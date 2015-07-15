a187d60c518019847c7dbe6f5cecd23309f7e130
vorobeyme <vorobeyme@gmail.com>
2015-07-15 22:33:13 +0300

    Slug test fixes

--- Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Utils/Jobeet.php
+++ Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Utils/Jobeet.php
@@ -7,7 +7,8 @@ class Jobeet
 	static public function slugify($text)
 	{
 		// replace all non letters or digits by -
-		$test = preg_replace('/W+/', '-', $text);
+		//$test = preg_replace('/W+/', '-', $text);
+		$text = str_replace(' ', '-', $text);
 
 		// trim and lowercase
 		$text = strtolower(trim($text, '-'));
