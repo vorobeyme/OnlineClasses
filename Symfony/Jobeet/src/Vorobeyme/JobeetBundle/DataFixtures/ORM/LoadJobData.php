3b71f0129a2d4705c20178de8b2d623edccead79
vorobeyme <vorobeyme@gmail.com>
2015-07-16 21:57:34 +0300

    Changed fixtures

--- Symfony/Jobeet/src/Vorobeyme/JobeetBundle/DataFixtures/ORM/LoadJobData.php
+++ Symfony/Jobeet/src/Vorobeyme/JobeetBundle/DataFixtures/ORM/LoadJobData.php
@@ -26,6 +26,7 @@ class LoadJobData extends AbstractFixture implements OrderedFixtureInterface
         $job_sensio_labs->setToken('job_sensio_labs');
         $job_sensio_labs->setEmail('job@example.com');
         $job_sensio_labs->setExpiresAt(new \DateTime('+30 days'));
+
         $job_extreme_sensio = new Job();
         $job_extreme_sensio->setCategory($em->merge($this->getReference('category-design')));
         $job_extreme_sensio->setType('part-time');
@@ -42,8 +43,25 @@ class LoadJobData extends AbstractFixture implements OrderedFixtureInterface
         $job_extreme_sensio->setEmail('job@example.com');
         $job_extreme_sensio->setExpiresAt(new \DateTime('+30 days'));
 
+        $job_expired = new Job();
+        $job_expired->setCategory($em->merge($this->getReference('category-programming')));
+        $job_expired->setType('full-time');
+        $job_expired->setCompany('Extreme Sensio');
+        $job_expired->setLogo('extreme-sensio.gif');
+        $job_expired->setUrl('http://www.extreme-sensio.com/');
+        $job_expired->setPosition('Web Developer Expired');
+        $job_expired->setLocation('Paris, France');
+        $job_expired->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.');
+        $job_expired->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
+        $job_expired->setIsPublic(true);
+        $job_expired->setIsActivated(true);
+        $job_expired->setToken('job_expired');
+        $job_expired->setEmail('job@example.com');
+        $job_expired->setExpiresAt(new \DateTime('2005-12-01'));
+
         $em->persist($job_sensio_labs);
         $em->persist($job_extreme_sensio);
+        $em->persist($job_expired);
         $em->flush();
     }
  
