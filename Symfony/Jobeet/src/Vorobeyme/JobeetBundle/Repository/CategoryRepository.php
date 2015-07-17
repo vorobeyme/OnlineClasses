6ea902e44711d86b631b29bb379d27257334f93e
vorobeyme <vorobeyme@gmail.com>
2015-07-17 21:53:49 +0300

    Added new method to Category repository

--- Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Repository/CategoryRepository.php
+++ Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Repository/CategoryRepository.php
@@ -12,4 +12,13 @@ use Doctrine\ORM\EntityRepository;
  */
 class CategoryRepository extends EntityRepository
 {
+	public function getWithJobs()
+    {
+        $query = $this->getEntityManager()->createQuery(
+            'SELECT c FROM VorobeymeJobeetBundle:Category c 
+             LEFT JOIN c.jobs j WHERE j.expires_at > :date'
+        )->setParameter('date', date('Y-m-d H:i:s', time()));
+ 
+        return $query->getResult();
+    }
 }
