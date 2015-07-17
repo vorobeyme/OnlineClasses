ba895ee5345b2b6d02aa1cf3124540cb447a934c
vorobeyme <vorobeyme@gmail.com>
2015-07-17 22:17:32 +0300

    Replaced hardcoded limit with config value

--- Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Controller/JobController.php
+++ Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Controller/JobController.php
@@ -26,7 +26,7 @@ class JobController extends Controller
         $categories = $em->getRepository('VorobeymeJobeetBundle:Category')->getWithJobs();
 
         foreach ($categories as $category) {
-            $category->setActiveJobs($em->getRepository('VorobeymeJobeetBundle:Job')->getActiveJobs($category->getId(), 10));
+            $category->setActiveJobs($em->getRepository('VorobeymeJobeetBundle:Job')->getActiveJobs($category->getId(), $this->container->getParameter('max_jobs_on_homepage')));
         }
 
         return $this->render('VorobeymeJobeetBundle:Job:index.html.twig', array(
