08b7508a767946911beeecd8e31caa60a4a07b29
vorobeyme <vorobeyme@gmail.com>
2015-07-20 22:53:13 +0300

    Created Category controller

---
+++ Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Controller/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Vorobeyme\JobeetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Vorobeyme\JobeetBundle\Entity\Category;
+
+/**
+ * Category controller
+ *
+ */
+class CategoryController extends Controller
+{
+
+}
