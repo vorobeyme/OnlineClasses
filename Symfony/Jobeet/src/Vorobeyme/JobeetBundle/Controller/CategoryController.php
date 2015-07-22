5bc0197eb31168d9e0fa62e8339f01644555b6c5
vorobeyme <vorobeyme@gmail.com>
2015-07-22 19:57:46 +0300

    Added 'showAction' to Category controller

--- Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Controller/CategoryController.php
+++ Symfony/Jobeet/src/Vorobeyme/JobeetBundle/Controller/CategoryController.php
@@ -11,5 +11,25 @@ use Vorobeyme\JobeetBundle\Entity\Category;
  */
 class CategoryController extends Controller
 {
+	/**
+	 * Finds and displays a Category entity
+	 *
+	 * @param string slug
+	 */
+	public function showAction($slug)
+	{
+		$em = $this->getDoctrine()->getManager();
 
+		$category = $em->getRepository('VorobeymeJobeetBundle:Category')->findOneBySlug($slug);
+
+		if (!$category) {
+			throw $this->createNotFoundException('Unable to find Category entity.');
+		}
+
+		$category->setActiveJobs($em->getRepository('VorobeymeJobeetBundle:Job')->getActiveJobs($category->getId()));
+
+		return $this->render('VorobeymeJobeetBundle:Category:show.html.twig', array(
+			'category' => $category,
+		));
+	}
 }
